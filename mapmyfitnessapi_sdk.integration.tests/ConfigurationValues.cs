4368674e3a4edf2331ea1245b2368f7e59c13e4e
Josh Rolstad <jrolstad@gmail.com>
2016-01-20 06:52:13 -0800

    Adding user specific configuration values

---
+++ mapmyfitnessapi_sdk.integration.tests/ConfigurationValues.cs
@@ -0,0 +1,11 @@
+﻿using System.Dynamic;
+
+namespace mapmyfitnessapi_sdk.integration.tests
+{
+    public static class ConfigurationValues
+    {
+        public const int OtherUserId = 1;
+        public const string ApiKey = "";
+        public const string AccessToken = "";
+    }
+}
