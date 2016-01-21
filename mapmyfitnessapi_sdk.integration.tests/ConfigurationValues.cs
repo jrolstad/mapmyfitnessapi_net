785e685f3e327f806d4e7264ca31ca7c7c4bbad6
Josh Rolstad <jrolstad@gmail.com>
2016-01-21 15:12:03 -0800

    Adding configuration values for test

--- mapmyfitnessapi_sdk.integration.tests/ConfigurationValues.cs
+++ mapmyfitnessapi_sdk.integration.tests/ConfigurationValues.cs
@@ -5,8 +5,9 @@ namespace mapmyfitnessapi_sdk.integration.tests
     public static class ConfigurationValues
     {
         public const int OtherUserId = 1;
-        public const string ApiKey = "";
-        public const string AccessToken = "";
+        public const string ApiKey = "1";
+        public const string AccessToken = "1";
         public const int UserGearId = 1;
+        public const int CurrentUserId = 1;
     }
 }
